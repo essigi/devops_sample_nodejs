5c7af32b6050e87457b6ea23e05f38bfc0af1a25
essigi <74293939+essigi@users.noreply.github.com>
2020-11-16 15:30:06 +0900

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,11 +3,11 @@ pipelineJob("Pipeline Basic Job") {
     def repo = "https://github.com/essigi/devops_sample_nodejs.git"
 
     description("Pipeline for $repo")
-    /*
+    
     triggers {
-        scm("H/5 * * * *")
+        scm('H/5 * * * *')
     }
-    */
+    
     definition {
         cpsScm {
             scm {
